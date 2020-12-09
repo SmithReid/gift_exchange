1db1df558bc469717855ffbf2420801f8eb7ee51
Reid Smith <smithreid001@gmail.com>
2020-12-08 19:41:23 -0500

    initial and likely final commit

---
+++ script.py
@@ -0,0 +1,28 @@
+from random import randint
+
+givers = {"Lena": None, 
+        "Reid": "Ashlye",
+        "Ashlye": "Reid", 
+        "Max": "Karley", 
+        "Karley": "Max", 
+        "Claire": "Jared",
+        "Jared": "Claire", 
+        "Nick": "Emily", 
+        "Emily": "Nick", 
+        "Chris": None
+        }
+
+if __name__ == "__main__":
+    final = {}
+    receivers = givers.copy()
+    list_givers = list(givers.keys())
+    for giver in list_givers:
+        receiver = list(receivers.keys())[randint(0, len(receivers.keys()) - 1)]
+        while receiver == givers[giver] or receiver == giver: 
+            receiver = list(receivers.keys())[randint(0, len(receivers.keys()) - 1)]
+        final[giver] = receiver
+        receivers.pop(receiver)
+
+        print("final: " + str(final))
+
+    print(len(set(final.keys())) == len(set(final.values())))
